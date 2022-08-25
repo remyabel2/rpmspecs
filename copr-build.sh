ab0ef310f013107bfaeac752a1ff0fc4bd983d04
Tommy Nguyen <remyabel@gmail.com>
2022-08-25 02:15:35 -0400

    Build everything in the same project

--- copr-build.sh
+++ copr-build.sh
@@ -21,16 +21,16 @@ pushd "${RPMBUILD_ROOT:-$PWD}"
         echo "Doesn't look like you're in the rpmbuild root. Exiting."
         exit 1
     fi
-    build_id=$(copr-cli build "${packages[0]}" "${srpms[0]}" --nowait)
+    build_id=$(copr-cli build "libimobiledevice" "${srpms[0]}" --nowait)
     build_id=$(echo "$build_id" | grep "Created" | tr -d ' ' | cut -f2 -d':')
-    for idx in "${!packages[@]}";
+    for idx in "${!srpms[@]}";
     do
         # Skip first one
         if [ "$idx" -eq 0 ];
         then
             continue
         fi
-        build_id=$(copr-cli build "${packages[$idx]}" "${srpms[$idx]}" --nowait --after-build-id="$build_id")
+        build_id=$(copr-cli build "libimobiledevice" "${srpms[$idx]}" --nowait --after-build-id="$build_id")
         build_id=$(echo "$build_id" | grep "Created" | tr -d ' ' | cut -f2 -d':')
     done
 popd
