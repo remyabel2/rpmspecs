b829c79a0eb10b19fa2a15c8d642d6c5a3881402
Tommy Nguyen <remyabel@gmail.com>
2022-09-09 04:56:44 -0400

    Format script

--- copr-build.sh
+++ copr-build.sh
@@ -7,8 +7,7 @@
 packages=("libplist" "libimobiledevice-glue" "libusbmuxd" "libimobiledevice" "usbmuxd")
 srpms=()
 
-for package in "${packages[@]}";
-do
+for package in "${packages[@]}"; do
     version=$(rpmspec -P "$package".spec | grep "Version:" | tr -s ' ' | cut -f2 -d' ')
     release=$(rpmspec -P "$package".spec | grep "Release:" | tr -s ' ' | cut -f2 -d' ')
     filename="$package-$version-$release.src.rpm"
@@ -16,21 +15,18 @@ do
 done
 
 pushd "${RPMBUILD_ROOT:-$PWD}"
-    if [ ! -d "SRPMS" ];
-    then
-        echo "Doesn't look like you're in the rpmbuild root. Exiting."
-        exit 1
+if [ ! -d "SRPMS" ]; then
+    echo "Doesn't look like you're in the rpmbuild root. Exiting."
+    exit 1
+fi
+build_id=$(copr-cli build "libimobiledevice" "${srpms[0]}" --nowait)
+build_id=$(echo "$build_id" | grep "Created" | tr -d ' ' | cut -f2 -d':')
+for idx in "${!srpms[@]}"; do
+    # Skip first one
+    if [ "$idx" -eq 0 ]; then
+        continue
     fi
-    build_id=$(copr-cli build "libimobiledevice" "${srpms[0]}" --nowait)
+    build_id=$(copr-cli build "libimobiledevice" "${srpms[$idx]}" --nowait --after-build-id="$build_id")
     build_id=$(echo "$build_id" | grep "Created" | tr -d ' ' | cut -f2 -d':')
-    for idx in "${!srpms[@]}";
-    do
-        # Skip first one
-        if [ "$idx" -eq 0 ];
-        then
-            continue
-        fi
-        build_id=$(copr-cli build "libimobiledevice" "${srpms[$idx]}" --nowait --after-build-id="$build_id")
-        build_id=$(echo "$build_id" | grep "Created" | tr -d ' ' | cut -f2 -d':')
-    done
+done
 popd
--- mock-build.sh
+++ mock-build.sh
@@ -7,8 +7,7 @@
 packages=("libplist" "libimobiledevice-glue" "libusbmuxd" "libimobiledevice" "usbmuxd")
 srpms=()
 
-for package in "${packages[@]}";
-do
+for package in "${packages[@]}"; do
     version=$(rpmspec -P "$package".spec | grep "Version:" | tr -s ' ' | cut -f2 -d' ')
     release=$(rpmspec -P "$package".spec | grep "Release:" | tr -s ' ' | cut -f2 -d' ')
     filename="$package-$version-$release.src.rpm"
@@ -16,24 +15,21 @@ do
 done
 
 pushd "${RPMBUILD_ROOT:-$PWD}"
-    if [ ! -d "SRPMS" ];
-    then
-        echo "Doesn't look like you're in the rpmbuild root. Exiting."
-        exit 1
-    fi
-    rm SRPMS/*.src.rpm
-    mock -r fedora-36-x86_64 --clean
-    mock -r fedora-36-x86_64 --init
-    for spec in SPECS/*.spec;
-    do
-        mock -r fedora-36-x86_64 --buildsrpm --spec "$spec" --sources SOURCES/ --resultdir SRPMS/
-    done
-    mock -r fedora-36-x86_64 --rebuild --chain "${srpms[@]}"
-    mock -r fedora-35-x86_64 --clean
-    mock -r fedora-35-x86_64 --init
-    for spec in SPECS/*.spec;
-    do
-        mock -r fedora-35-x86_64 --buildsrpm --spec "$spec" --sources SOURCES/ --resultdir SRPMS/
-    done
-    mock -r fedora-35-x86_64 --rebuild --chain "${srpms[@]}"
+if [ ! -d "SRPMS" ]; then
+    echo "Doesn't look like you're in the rpmbuild root. Exiting."
+    exit 1
+fi
+rm SRPMS/*.src.rpm
+mock -r fedora-36-x86_64 --clean
+mock -r fedora-36-x86_64 --init
+for spec in SPECS/*.spec; do
+    mock -r fedora-36-x86_64 --buildsrpm --spec "$spec" --sources SOURCES/ --resultdir SRPMS/
+done
+mock -r fedora-36-x86_64 --rebuild --chain "${srpms[@]}"
+mock -r fedora-35-x86_64 --clean
+mock -r fedora-35-x86_64 --init
+for spec in SPECS/*.spec; do
+    mock -r fedora-35-x86_64 --buildsrpm --spec "$spec" --sources SOURCES/ --resultdir SRPMS/
+done
+mock -r fedora-35-x86_64 --rebuild --chain "${srpms[@]}"
 popd
